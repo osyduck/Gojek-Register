55cb570e26782ac6fbf082e3fee11fa4ed03dc2b
Janu Yoga <onlypaymenthere@gmail.com>
2019-03-26 21:08:25 +0700

    Create function.php

---
+++ function.php
@@ -0,0 +1,46 @@
+<?php
+function request($url, $token = null, $data = null, $pin = null){
+    
+$header[] = "Host: api.gojekapi.com";
+$header[] = "User-Agent: okhttp/3.10.0";
+$header[] = "Accept: application/json";
+$header[] = "Accept-Language: en-ID";
+$header[] = "Content-Type: application/json; charset=UTF-8";
+$header[] = "X-AppVersion: 3.16.1";
+$header[] = "X-UniqueId: 747e741cee856b1c";
+$header[] = "Connection: keep-alive";    
+if ($pin):
+$header[] = "pin: $pin";    
+    endif;
+if ($token):
+$header[] = "Authorization: Bearer $token";
+endif;
+$c = curl_init("https://api.gojekapi.com".$url);
+    curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+    if ($data):
+    curl_setopt($c, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($c, CURLOPT_POST, true);
+    endif;
+    curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($c, CURLOPT_HEADER, true);
+    curl_setopt($c, CURLOPT_HTTPHEADER, $header);
+    if ($socks):
+          curl_setopt($c, CURLOPT_HTTPPROXYTUNNEL, true); 
+          curl_setopt($c, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5); 
+          curl_setopt($c, CURLOPT_PROXY, $socks);
+        endif; 
+    $response = curl_exec($c);
+    $httpcode = curl_getinfo($c);
+    if (!$httpcode)
+        return false;
+    else {
+        $header = substr($response, 0, curl_getinfo($c, CURLINFO_HEADER_SIZE));
+        $body   = substr($response, curl_getinfo($c, CURLINFO_HEADER_SIZE));
+    }
+    $json = json_decode($body, true);
+    return $json;
+}
+
+?>
