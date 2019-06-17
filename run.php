e280198fe850f73875766a2b2adf5e7f7c575df9
Janu Yoga <onlypaymenthere@gmail.com>
2019-06-17 07:54:00 +0700

    Update run.php

--- run.php
+++ run.php
@@ -23,12 +23,14 @@ function register($no)
 	$email = str_replace(" ", "", $nama) . mt_rand(100, 999);
 	$data = '{"name":"' . nama() . '","email":"' . $email . '@gmail.com","phone":"+' . $no . '","signed_up_country":"ID"}';
 	$register = request("/v5/customers", "", $data);
+	//print_r($register);
 	if ($register['success'] == 1)
 		{
 		return $register['data']['otp_token'];
 		}
 	  else
 		{
+      save("error_log.txt", json_encode($register));
 		return false;
 		}
 	}
@@ -42,12 +44,44 @@ function verif($otp, $token)
 		}
 	  else
 		{
+      save("error_log.txt", json_encode($verif));
+		return false;
+		}
+	}
+	function login($no)
+	{
+	$nama = nama();
+	$email = str_replace(" ", "", $nama) . mt_rand(100, 999);
+	$data = '{"phone":"+'.$no.'"}';
+	$register = request("/v4/customers/login_with_phone", "", $data);
+	//print_r($register);
+	if ($register['success'] == 1)
+		{
+		return $register['data']['login_token'];
+		}
+	  else
+		{
+      save("error_log.txt", json_encode($register));
+		return false;
+		}
+	}
+function veriflogin($otp, $token)
+	{
+	$data = '{"client_name":"gojek:cons:android","client_secret":"83415d06-ec4e-11e6-a41b-6c40088ab51e","data":{"otp":"'.$otp.'","otp_token":"'.$token.'"},"grant_type":"otp","scopes":"gojek:customer:transaction gojek:customer:readonly"}';
+	$verif = request("/v4/customers/login/verify", "", $data);
+	if ($verif['success'] == 1)
+		{
+		return $verif['data']['access_token'];
+		}
+	  else
+		{
+      save("error_log.txt", json_encode($verif));
 		return false;
 		}
 	}
 function claim($token)
 	{
-	$data = '{"promo_code":"GOFOODMAKANMURAH"}';
+	$data = '{"promo_code":"GOFOODHEMAT1"}';
 	$claim = request("/go-promotions/v1/promotions/enrollments", $token, $data);
 	if ($claim['success'] == 1)
 		{
@@ -55,16 +89,21 @@ function claim($token)
 		}
 	  else
 		{
+      save("error_log.txt", json_encode($claim));
 		return false;
 		}
 	}
+echo "Choose Login or Register? Login = 1 & Register = 2: ";
+$type = trim(fgets(STDIN));
+if($type == 2){
+echo "It's Register Way\n";
 echo "Input 62 For ID and 1 For US Phone Number\n";
 echo "Enter Number: ";
 $nope = trim(fgets(STDIN));
 $register = register($nope);
 if ($register == false)
 	{
-	echo "Failed to Get OTP!\n";
+	echo "Failed to Get OTP, Use Unregistered Number!\n";
 	}
   else
 	{
@@ -90,4 +129,39 @@ if ($register == false)
 			}
 		}
 	}
+}else if($type == 1){
+echo "It's Login Way\n";
+echo "Input 62 For ID and 1 For US Phone Number\n";
+echo "Enter Number: ";
+$nope = trim(fgets(STDIN));
+$login = login($nope);
+if ($login == false)
+	{
+	echo "Failed to Get OTP!\n";
+	}
+  else
+	{
+	echo "Enter Your OTP: ";
+	// echo "Enter Number: ";
+	$otp = trim(fgets(STDIN));
+	$verif = veriflogin($otp, $login);
+	if ($verif == false)
+		{
+		echo "Failed to Login with Your Number!\n";
+		}
+	  else
+		{
+		echo "Ready to Claim\n";
+		$claim = claim($verif);
+		if ($claim == false)
+			{
+			echo "Failed to Claim Voucher, Try to Claim Manually\n";
+			}
+		  else
+			{
+			echo $claim . "\n";
+			}
+		}
+	}
+}
 ?>
